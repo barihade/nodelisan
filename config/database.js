2fe71f62568461544dfce95180c1e8522cc84359
Bari Hade <barihade@gmail.com>
2019-05-16 14:41:44 +0700

    Untuk Sidang Skripsi

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+    database:'mongodb://localhost:27017/nodelisandb',
+    //database:'mongodb://nodelisandb:d987f7df2d77a51c8e50690b623ee604@dokku-mongo-nodelisandb:27017/nodelisandb',
+    secret: 'yoursecret'
+}
